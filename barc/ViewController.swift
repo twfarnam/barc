496e6687ffcbecd6aab0f8e60000d0ed44bd46b3
Tim Farnam <twfarnam@gmail.com>
2017-10-31 08:39:28 -0600

    Initial Commit

---
+++ barc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  barc
+//
+//  Created by Tim Farnam on 10/31/17.
+//  Copyright © 2017 Tim Farnam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
